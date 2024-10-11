3ddbce9e6423918855ebaec61cab407e8e9d0061
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-11 16:40:38 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+[ ! -d /root/backup ] && mkdir /root/backup
+cp -r /root/verifier/configs /root/backup/carv
+
 cd ~
 rm -r verifier
 git clone https://github.com/carv-protocol/verifier.git
