183fa7cb7c10926bdf019d7fc8100e84c3886a8e
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-15 02:16:22 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -15,18 +15,29 @@ folder=$(echo $path | awk -F/ '{print $NF}')
 json=~/logs/report-$folder
 source ~/.bash_profile
 
-version=$(echo {$(sudo journalctl -u carv-verifier.service | grep version | tail -1 | cut -d '{' -f 2-) | jq -r '."service.version"')
-service=$(sudo systemctl status carv-verifier --no-pager | grep "active (running)" | wc -l)
-last=$(sudo journalctl -u carv-verifier.service --no-hostname -o cat | grep "tx hash" | tail -1 | jq -r .ts)
+#version=$(echo {$(sudo journalctl -u carv-verifier.service | grep version | tail -1 | cut -d '{' -f 2-) | jq -r '."service.version"')
+#service=$(sudo systemctl status carv-verifier --no-pager | grep "active (running)" | wc -l)
+#last=$(sudo journalctl -u carv-verifier.service --no-hostname -o cat | grep "tx hash" | tail -1 | jq -r .ts)
 
-if [ $service -ne 1 ]
-then
-  status="error";
-  message="service not running"
-else
-  status="ok";
-  message="last tx: "$(hdate $last)
-fi
+#if [ $service -ne 1 ]
+#then
+#  status="error";
+#  message="service not running"
+#else
+#  status="ok";
+#  message="last tx: "$(hdate $last)
+#fi
+
+tail=100000
+container=$(docker ps | grep carv | awk '{print $NF}')
+[ $container ] && docker_status=$(docker inspect $container | jq -r .[].State.Status)
+errors=$(docker logs --tail $tail $container 2>&1 | grep $(date --utc +%F) | grep -c ERROR)
+
+case $docker_status in
+  running) status=ok; message="errors $errors" ;;
+  restarting) status=warning; message="docker is restarting" ;;
+  *) status=error; message="docker not running" ;;
+esac
 
 cat >$json << EOF
 {
@@ -44,7 +55,7 @@ cat >$json << EOF
       "version":"$version",
       "status":"$status",
       "message":"$message",
-      "service":$service
+      "errors":$errors
   }
 }
 EOF
