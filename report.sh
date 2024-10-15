dfd0f8966ad05d20abdc171f172af0dcbc182988
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-15 02:30:28 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -5,19 +5,7 @@ folder=$(echo $path | awk -F/ '{print $NF}')
 json=~/logs/report-$folder
 source ~/.bash_profile
 
-#version=$(echo {$(sudo journalctl -u carv-verifier.service | grep version | tail -1 | cut -d '{' -f 2-) | jq -r '."service.version"')
-#service=$(sudo systemctl status carv-verifier --no-pager | grep "active (running)" | wc -l)
-#last=$(sudo journalctl -u carv-verifier.service --no-hostname -o cat | grep "tx hash" | tail -1 | jq -r .ts)
-
-#if [ $service -ne 1 ]
-#then
-#  status="error";
-#  message="service not running"
-#else
-#  status="ok";
-#  message="last tx: "$(hdate $last)
-#fi
-
+docker_status=$(docker inspect verifier | jq -r .[].State.Status)
 errors=$(docker logs verifier 2>&1 | grep $(date --utc +%F) | grep -c ERROR)
 
 case $docker_status in
