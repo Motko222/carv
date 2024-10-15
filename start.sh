6458c94177118dd9972af6031e7d5773396a04be
Motko <77466238+Motko222@users.noreply.github.com>
2024-10-15 02:19:30 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,9 @@
 
 source ~/.bash_profile
 
-sudo systemctl restart carv-verifier
-sudo journalctl -u carv-verifier.service -f --no-hostname -o cat
+#sudo systemctl restart carv-verifier
+#sudo journalctl -u carv-verifier.service -f --no-hostname -o cat
+
+docker stop verifier
+docker rm verifier
+docker run -d --name verifier --restart always -v /root/verifier/configs:/data/conf  carvprotocol/verifier:mainnet-amd64
